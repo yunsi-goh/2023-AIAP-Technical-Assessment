b56e1bf1f9356168808602b580f4f5d4e45081d3
Yun Si Goh <gohgys97@gmail.com>
2023-09-18 04:04:52 +0800

    Add script to load data

---
+++ src/load.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import config as cfg
+from sklearn.model_selection import train_test_split
+
+def load_preprocessed(file_path):
+
+    # Load data
+    data = pd.read_csv(file_path)
+    data = data.head(100)
+
+    # Split features and target
+    target = "ticket"
+    X = data.drop([target], axis="columns")
+    y = data[target]
+
+    # Split data into training (70%) and testing (30%) sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=cfg.test_size,
+                                                        random_state=cfg.random_state)
+
+    return X_train, X_test, y_train, y_test
